c119cbb63708309fe5a6d2627ba6a48b46fd7918
SaulZQ1030 <112032991+SaulZQ1030@users.noreply.github.com>
2023-10-04 15:44:47 -0600

    alumnos y profesor

--- alumno/Alumno.java
+++ alumno/Alumno.java
@@ -1,5 +1,40 @@
 package alumno;
 
 public class Alumno {
+    //Atributos
+    private String nombre;
+    private String apellido;
+    private int numeroDeLista;
+    private float Promedio;
+    private int Edad;
+    private static int AlumnosTotales;
+
+    //Constructores
+    public Alumno(String nombre, String apellido, int Edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Edad = Edad;
+    }
+    
+    //metodos
+    public String getNombre(){
+        return this.nombre + " " + this.apellido;
+    }
+    public int getEdad(){
+        return this.Edad;
+    }
+    public float getPromedio(){
+        return this.Promedio;
+    }
+    public int getNumeroAlumnosTotales(){
+        return AlumnosTotales;
+    }
+    public void setNumeroLista(int numeroDeLista){
+        this.numeroDeLista = numeroDeLista;
+    }
+    public int getNumeroLista(){
+        return this.numeroDeLista;
+    }
+
     
 }
--- profesor/Profesor.java
+++ profesor/Profesor.java
@@ -1,5 +1,56 @@
 package profesor;
 
+import alumno.Alumno;
+import java.util.*;
+
+
 public class Profesor {
-    
+    //atributos
+    private String nombre;
+    private String apellido;
+    private int Edad;
+    private float CalificacionAprobatoria;
+    private int AlumnosAprobados;
+    private int AlumnosReprobados;
+    private static int numeroDeProfesores;
+
+    //constructores
+    public Profesor(String nombre, String apellido, int Edad, float CalificacionAprobatoria){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Edad = Edad;
+        this.CalificacionAprobatoria = CalificacionAprobatoria;
+    }
+
+    //metodos
+    public String getNombre(){
+        return this.nombre + " " + this.apellido;
+    }
+    public float getCalificaiconAprobatoria(){
+        return this.CalificacionAprobatoria;
+    }
+    public int getNumeroProfesores(){
+        return numeroDeProfesores;
+    }
+
+
+    public int AlumnosAprobados(ArrayList<Alumno> ListaDeAlumnos){
+        this.AlumnosAprobados = 0;
+        for(Alumno alumno : ListaDeAlumnos ){
+            if(alumno.getPromedio() >= this.CalificacionAprobatoria){
+                this.AlumnosAprobados ++;
+            }
+        }
+        return this.AlumnosAprobados;
+    }
+    public int AlumnosReprobados(ArrayList<Alumno> ListaDeAlumnos){
+        this.AlumnosReprobados = 0;
+        for(Alumno alumno : ListaDeAlumnos ){
+            if(alumno.getPromedio() < this.CalificacionAprobatoria){
+                this.AlumnosReprobados ++;
+            }
+        }
+        return this.AlumnosReprobados;
+    }
+
 }
